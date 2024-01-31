da4fa03e70c319462199929c3b4da2b633d6df5b
Nafim Ahmed <recentnafimahmed@gmail.com>
2024-01-31 15:39:56 +0600

    home fixed

--- lib/home.dart
+++ lib/home.dart
@@ -2,14 +2,15 @@
 
 
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget{
+  const Home({super.key});
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
+    return const Scaffold(
       body: Column(
         children: [],
       ),
